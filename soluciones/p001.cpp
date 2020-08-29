4d5e051bd15f7e713588b089d25e371615623792
Alvasanb <sanbalvaro@hotmail.com>
2020-08-29 12:52:18 +0200

    Solución v2

--- soluciones/p001.cpp
+++ soluciones/p001.cpp
@@ -3,17 +3,41 @@ using namespace std;
 const int DIM = 1000;
 
 int main() {
-  // We need to get the multiples of 3 or 5 below 1000
-  // If we have the numbers below 1000 and we want to get the multiples of 3, we just see what of these numbers return 0 when %3, same with 5
-  // Other way to get the numbers is multiply the natural numbers by 3 or 5 until they are grater than 1000
-
-  //Bad solution
+  int n = 3;
   int sum = 0;
-  for (int i = 1; i < DIM; i++){
-    if (i%3 == 0 || i%5 == 0){
-      sum += i;
-    }
-  }
-  cout << sum << endl;
+  while (n < DIM){
+    sum += n;
+    n += 3;
+  } //Se ejecuta 333 con DIM = 1000
+  n = 15;
+  sum += 15; //Sumamos los dos primeros multiplos de 5 a sum, no se sumarán en el while de abajo.
+  while (n < DIM){
+    n += 15;
+    sum += n-10;
+    sum += n-5;
+  } //Sumamos los multiplos de 5 sin contar los que son también multiplos de 3. Para DIM = 1000 se ejecutará 66 veces
+  cout << sum-DIM << endl; //En el while de arriba se suma 1000 cuando no se debería.
+
   return 0;
 }
+
+
+
+
+
+/*
+// No se si es mejor solución la de arriba pero me parece que si. Por lo que se los if son lentos.
+// We need to get the multiples of 3 or 5 below 1000
+// If we have the numbers below 1000 and we want to get the multiples of 3, we just see what of these numbers return 0 when %3, same with 5
+// Other way to get the numbers is multiply the natural numbers by 3 or 5 until they are grater than 1000
+
+//Bad solution
+int sum = 0;
+for (int i = 1; i < DIM; i++){
+  if (i%3 == 0 || i%5 == 0){
+    sum += i;
+  }
+}
+cout << sum << endl;
+return 0;
+*/
